1d6ae9f1ace0c290ca67975bffcfa95d0bf84ac7
JoeltheSpider <joeloffbeat@gmail.com>
2021-04-09 23:31:47 +0530

    Init Pre process module

---
+++ pre_processing/trade_preprocess.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+class PreProcessTrade():
+    def __init__(self, console):
+        self.console = console
+        # no use of extract process from
+
+    def preprocess_player_trade(self, players):
+        pass 
